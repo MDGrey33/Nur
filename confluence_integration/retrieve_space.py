00164142ef36ca79a7e2144c72252d14a7c4334a
Roland Abou Younes <roland@abouyounes.com>
2023-12-01 22:38:02 +0200

    Space retrieval data writing to database

--- confluence_integration/retrieve_space.py
+++ confluence_integration/retrieve_space.py
@@ -1,7 +1,8 @@
-import requests
 import json
+from bs4 import BeautifulSoup
 from atlassian import Confluence
 from credentials import confluence_credentials
+from database.confluence_database import store_space_data, store_pages_data
 
 # Initialize Confluence API
 confluence = Confluence(
@@ -10,13 +11,16 @@ confluence = Confluence(
     password=confluence_credentials['api_token']
 )
 
+
 def get_top_level_ids(space_key):
     top_level_pages = confluence.get_all_pages_from_space(space_key)
     return [page['id'] for page in top_level_pages]
 
+
 def get_child_ids(item_id, content_type):
     child_items = confluence.get_page_child_by_type(item_id, type=content_type)
     return [child['id'] for child in child_items]
+
 
 def get_all_pages_recursive(space_key):
     def get_child_pages_recursively(page_id):
@@ -34,7 +38,6 @@ def get_all_pages_recursive(space_key):
         all_pages.extend(get_child_pages_recursively(top_level_id))
 
     return all_pages
-
 
 
 def get_all_comments_recursive(page_id):
@@ -66,32 +69,48 @@ def get_space_ids(space_key):
     print("Page and Comments Map:", page_comments_map)
 
 
-def get_all_space_content(space_key):
+def choose_space():
+    spaces = confluence.get_all_spaces(start=0, limit=50, expand='description.plain,body.view,value')
+    for i, space in enumerate(spaces['results']):
+        print(f"{i + 1}. {space['name']} (Key: {space['key']})")
+    choice = int(input("Choose a space (number): ")) - 1
+    space_key = spaces['results'][choice]['key']
+    space_data={'space_key': space_key,
+                'url': confluence_credentials['base_url'],
+                'login': confluence_credentials['username'],
+                'token': confluence_credentials['api_token']
+                }
+    store_space_data(space_data)
+    return spaces['results'][choice]['key']
+
+
+def strip_html_tags(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    return soup.get_text()
+
+
+def get_space_content():
+    space_key = choose_space()
     all_page_ids = get_all_pages_recursive(space_key)
     page_content_map = {}
 
     for page_id in all_page_ids:
-        # Include history and version details to get author and date/time information
         page = confluence.get_page_by_id(page_id, expand='body.storage,history,version')
-        page_title = page['title']
+        page_title = strip_html_tags(page['title'])
 
-        # Fetch author and date/time information
         page_author = page['history']['createdBy']['displayName']
         created_date = page['history']['createdDate']
         last_updated = page['version']['when']
 
-        # Fetch page content
-        page_content = page.get('body', {}).get('storage', {}).get('value', '')
+        page_content = strip_html_tags(page.get('body', {}).get('storage', {}).get('value', ''))
 
-        # Fetch and concatenate comments
         page_comments_content = ""
         page_comments = get_all_comments_recursive(page_id)
         for comment_id in page_comments:
             comment = confluence.get_page_by_id(comment_id, expand='body.storage')
             comment_content = comment.get('body', {}).get('storage', {}).get('value', '')
-            page_comments_content += comment_content
+            page_comments_content += strip_html_tags(comment_content)
 
-        # Map the required details to the page ID
         page_content_map[page_id] = {
             'title': page_title,
             'author': page_author,
@@ -101,22 +120,14 @@ def get_all_space_content(space_key):
             'comments': page_comments_content
         }
 
-    # Write to JSON file
     with open('page_content.json', 'w') as f:
         json.dump(page_content_map, f)
+
+    store_pages_data(space_key, page_content_map)
+    print("Page content written to JSON and database.")
 
     return page_content_map
 
 
-def choose_space():
-    spaces = confluence.get_all_spaces(start=0, limit=50, expand='description.plain,body.view,value')
-    for i, space in enumerate(spaces['results']):
-        print(f"{i + 1}. {space['name']} (Key: {space['key']})")
-    choice = int(input("Choose a space (number): ")) - 1
-    return spaces['results'][choice]['key']
-
-
 if __name__ == "__main__":
-    space_key = choose_space()
-    get_all_space_content(space_key)
-
+    get_space_content()
---
+++ database/confluence_database.py
@@ -0,0 +1,74 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+# Define the base class
+Base = declarative_base()
+
+
+# Define the SpaceData model
+class SpaceData(Base):
+    __tablename__ = 'space_data'
+
+    id = Column(Integer, primary_key=True)
+    space_key = Column(String)
+    url = Column(String)
+    login = Column(String)
+    token = Column(String)
+
+
+# Define the PageData model
+class PageData(Base):
+    __tablename__ = 'page_data'
+
+    id = Column(Integer, primary_key=True)
+    page_id = Column(String)
+    space_key = Column(String)
+    title = Column(String)
+    author = Column(String)
+    createdDate = Column(DateTime)
+    lastUpdated = Column(DateTime)
+    content = Column(Text)
+    comments = Column(Text)
+
+
+# Setup database connection
+engine = create_engine('sqlite:///../database/confluence_data.db')
+Base.metadata.bind = engine
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def store_space_data(space_data):
+    new_space = SpaceData(space_key=space_data['space_key'],
+                          url=space_data['url'],
+                          login=space_data['login'],
+                          token=space_data['token'])
+    session.add(new_space)
+    session.commit()
+    print(f"Space data written to database")
+
+
+def parse_datetime(date_string):
+    return datetime.fromisoformat(date_string.replace('Z', '+00:00'))
+
+
+def store_pages_data(space_key, pages_data):
+    for page_id, page_info in pages_data.items():
+        created_date = parse_datetime(page_info['createdDate'])
+        last_updated = parse_datetime(page_info['lastUpdated'])
+
+        new_page = PageData(page_id=page_id,
+                            space_key=space_key,
+                            title=page_info['title'],
+                            author=page_info['author'],
+                            createdDate=created_date,
+                            lastUpdated=last_updated,
+                            content=page_info['content'],
+                            comments=page_info['comments'])
+        session.add(new_page)
+    session.commit()
+    print("Page content written to database.")
+
