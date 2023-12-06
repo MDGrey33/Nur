ddf82b4407c1e5b27ce3b354f9b1bf842f9c1461
Roland Abou Younes <roland@abouyounes.com>
2023-12-06 21:11:58 +0200

    Added configuration file

---
+++ configuration.py
@@ -0,0 +1,8 @@
+# /Users/roland/code/Nur/configuration.py
+
+project_path = "/Users/roland/code/Nur"
+# build file_system_path and database_path from project_path
+file_system_path = project_path + "/content/file_system"
+database_path = project_path + "/content/database"
+vector_folder_path = database_path + "/confluence_page_vectors"
+sql_file_path = database_path + "/confluence_pages_sql.db"
