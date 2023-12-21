8204d07f3f1d176bbf14a52b770b2b8482c61148
Roland Abou Younes <roland@abouyounes.com>
2023-12-21 18:31:21 +0200

    Persisted processed message list even after reboot

--- slack/event_consumer.py
+++ slack/event_consumer.py
@@ -1,3 +1,4 @@
+import json
 from slack.event_publisher import EventPublisher
 from slack_sdk import WebClient
 from credentials import slack_bot_user_oauth_token
@@ -8,8 +9,20 @@ class EventConsumer:
     def __init__(self, publisher: EventPublisher):
         self.publisher = publisher
         self.web_client = WebClient(token=slack_bot_user_oauth_token)
-        self.processed_message_ids = set()  # Set to keep track of processed message IDs
+        self.processed_ids_file = 'processed_ids.json'
+        self.load_processed_message_ids()
         self.bot_user_id = self.web_client.auth_test().get('user_id')  # Get the bot user ID
+
+    def load_processed_message_ids(self):
+        try:
+            with open(self.processed_ids_file, 'r') as file:
+                self.processed_message_ids = set(json.load(file))
+        except (FileNotFoundError, json.JSONDecodeError):
+            self.processed_message_ids = set()
+
+    def save_processed_message_ids(self):
+        with open(self.processed_ids_file, 'w') as file:
+            json.dump(list(self.processed_message_ids), file)
 
     def process_message_event(self, event_data):
         print("Processing new event:", event_data)  # Detailed diagnostic output
@@ -17,14 +30,9 @@ class EventConsumer:
         message_id = event_data.get("client_msg_id")
         user_id = event_data.get("user")
 
-        # Skip if the message is from the bot itself
-        if user_id == self.bot_user_id:
-            print(f"Skipping bot's own message: {message_id}")
-            return
-
-        # Skip if the message has been processed already
-        if message_id in self.processed_message_ids:
-            print(f"Duplicate message detected and skipped: {message_id}")
+        # Skip if the message is from the bot itself or already processed
+        if user_id == self.bot_user_id or message_id in self.processed_message_ids:
+            print(f"Skipping message: {message_id}")
             return
 
         # Validate required keys directly from the event_data
@@ -33,31 +41,23 @@ class EventConsumer:
             text = event_data["text"]
             thread_ts = event_data["ts"]
 
-            print(f"Event details - User: {user_id}, Channel: {channel_id}, Text: {text}")
-
             # Determine the type of message and generate an appropriate response
             if "?" in text:  # Check if the message is a question
-                print("Identified message as a question.")
                 response_text = self.generate_response(text)
             else:
-                print("Identified message as not a question.")
                 response_text = f"Received your message: '{text}'"
 
             # Post response back to Slack if all required information is present
             if channel_id and thread_ts and response_text:
-                print(f"Posting response to channel {channel_id}")
                 self.web_client.chat_postMessage(channel=channel_id, text=response_text, thread_ts=thread_ts)
-            else:
-                print("Missing information required for posting response:",
-                      {"channel_id": channel_id, "thread_ts": thread_ts, "response_text": response_text})
 
             # Mark this message as processed
             self.processed_message_ids.add(message_id)
+            self.save_processed_message_ids()
         else:
             print("Event does not contain expected keys:", event_data)
 
     def generate_response(self, question):
-        # Logic for generating a response using document retrieval and GPT-4T
         file_name = "context.txt"  # Placeholder for actual context retrieval logic
         relevant_document_ids = retrieve_relevant_documents(question)
         response_text = query_gpt_4t_with_context(question, relevant_document_ids)
@@ -67,21 +67,19 @@ class EventConsumer:
         while not self.publisher.message_queue.empty():
             message_event = self.publisher.message_queue.get()
             self.process_message_event(message_event)
-            self.publisher.message_queue.task_done()  # Acknowledge processing
+            self.publisher.message_queue.task_done()
 
     def consume_reactions(self):
         while not self.publisher.reaction_queue.empty():
             reaction_event = self.publisher.reaction_queue.get()
-            print("Processing reaction event:", reaction_event)  # Placeholder for actual reaction processing logic
-            self.publisher.reaction_queue.task_done()  # Acknowledge processing
-
+            print("Processing reaction event:", reaction_event)
+            self.publisher.reaction_queue.task_done()
 
 def consume_events():
     publisher = EventPublisher()
     consumer = EventConsumer(publisher)
     consumer.consume_messages()
     consumer.consume_reactions()
-
 
 if __name__ == "__main__":
     consume_events()
