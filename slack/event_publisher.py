3300a2b76e708af66402b27f7842e6b365007e4d
Roland Abou Younes <roland@abouyounes.com>
2024-01-23 11:24:03 +0200

    Making event publisher thread safe

--- slack/event_publisher.py
+++ slack/event_publisher.py
@@ -1,5 +1,6 @@
 # ./slack/event_publisher.py
 import os
+import fcntl
 from persistqueue import Queue
 from configuration import persist_question_queue_path, persist_feedback_queue_path
 
@@ -8,11 +9,23 @@ class EventPublisher:
     def __init__(self):
         self.question_queue = Queue(os.path.join(persist_question_queue_path, "question_events"))
         self.feedback_queue = Queue(os.path.join(persist_feedback_queue_path, "feedback_events"))
+        # File paths for locking
+        self.question_queue_lock_path = os.path.join(persist_question_queue_path, "question_events_lock")
+        self.feedback_queue_lock_path = os.path.join(persist_feedback_queue_path, "feedback_events_lock")
+        # Create lock files
+        open(self.question_queue_lock_path, 'a').close()
+        open(self.feedback_queue_lock_path, 'a').close()
 
     def publish_new_question(self, question_event):
-        self.question_queue.put(question_event)
+        with open(self.question_queue_lock_path, 'r+') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
+            self.question_queue.put(question_event)
+            fcntl.flock(f, fcntl.LOCK_UN)
         print(f"New question event enqueued: {question_event}")
 
     def publish_new_feedback(self, feedback_event):
-        self.feedback_queue.put(feedback_event)
+        with open(self.feedback_queue_lock_path, 'r+') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
+            self.feedback_queue.put(feedback_event)
+            fcntl.flock(f, fcntl.LOCK_UN)
         print(f"New feedback event enqueued: {feedback_event}")
