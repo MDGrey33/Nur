50dd7a5cb17e6be2db0bedbcc52d8004cc4c6bd6
Roland Abou Younes <roland@abouyounes.com>
2024-02-04 17:11:33 +0200

    Assistant instruction updated to make it usable directly in slack without parsing

--- oai_assistants/assistant_instruction.py
+++ oai_assistants/assistant_instruction.py
@@ -7,8 +7,8 @@ Refrain from improvisation or sourcing content from external files.
 Utilize logical reasoning and deduction based on the conversation history and previous context.
 If you lack an answer from the files, clearly state it and abstain from responding.
 Disclose when using external knowledge to explain information beyond the provided files.
-Format your responses using only the following keys:  
-summary: 
-comprehensive answer: 
-technical trace: 
+Format your responses as follows:  
+summary: [providing a short to the point answer]
+comprehensive answer: [providing a detailed answer]
+technical trace: [providing the source of the information]
 """
