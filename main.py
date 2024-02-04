4582f5c5c22c77793aea94a8d322e2ee7132bc20
Roland Abou Younes <roland@abouyounes.com>
2024-02-04 12:58:44 +0200

    Removed load slackbot parallel module cause its redundant

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from confluence_integration.retrieve_space import get_space_content
 from vector.chroma_threads import retrieve_relevant_documents, add_to_vector, retrieve_relevant_documents_with_proximity
 from oai_assistants.query_assistant_from_documents import query_assistant_with_context
 from gpt_4t.query_from_documents_threads import query_gpt_4t_with_context
-from slack.channel_interaction_threads import load_slack_bot_parallel
+from slack.channel_interaction_threads import load_slack_bot
 from confluence_integration.extract_page_content_and_store_processor import get_page_content_using_queue
 from vector.vectorize_and_persist_processor import process_vectorization_queue
 from qa_syncup.sync_up_qa_articles_to_confluence import sync_up_interactions_to_confluence
@@ -36,11 +36,11 @@ def main_menu():
     while True:
         print("\nMain Menu:")
         print("1. Load New Documentation Space")
-        print("2. Ask a Question to GPT-dT Assistant")
+        print("2. Ask a Question to GPT-4T Assistant")
         print("3. Ask a question to GPT-4T")
-        print("4. Start Slack Bot")
+        print("4. Ask a Question with proximity to GPT-4T")
         print("5. Sync up QA articles to Confluence")
-        print("6. Ask a Question with proximity to GPT-4T")
+        print("6. Start Slack Bot")
         print("0. Cancel/Quit")
         choice = input("Enter your choice (0-6): ")
 
@@ -49,28 +49,34 @@ def main_menu():
             space_key = get_page_content_using_queue(space_key)
             process_vectorization_queue(space_key)
             print("\nSpace retrieval and indexing complete.")
+
         elif choice == "2":
             question = ask_question()
             if question:
                 answer, thread_id = answer_question_with_assistant(question)
                 print(f"\nThread ID: {thread_id}\nAnswer: {answer}")
+
         elif choice == "3":
             question = ask_question()
             if question:
                 answer = answer_question_with_gpt_4t(question)
                 print("\nAnswer:", answer)
+
         elif choice == "4":
-            print("Starting Slack Bot Parallel Processing...")
-            load_slack_bot_parallel()
-            print("Slack Bot is running in parallel processing mode.")
-        elif choice == "5":
-            print("Syncing up QA articles to Confluence...")
-            sync_up_interactions_to_confluence()
-        elif choice == "6":
             question = ask_question()
             if question:
                 answer = answer_question_with_gpt_4t_10(question)
                 print("\nAnswer:", answer)
+
+        elif choice == "5":
+            print("Syncing up QA articles to Confluence...")
+            sync_up_interactions_to_confluence()
+
+        elif choice == "6":
+            print("Starting Slack Bot...")
+            load_slack_bot()
+            print("Slack Bot is running in parallel processing mode.")
+
         elif choice == "0":
             print("Exiting program.")
             break
