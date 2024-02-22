4383a7f2ee6bbfd76c0502d982702d8ef5b96bf9
Roland Abou Younes <roland@abouyounes.com>
2024-02-22 17:32:13 +0200

    Added comments to the credentials file

--- credentials_example.py
+++ credentials_example.py
@@ -1,14 +1,19 @@
 # ./credentials.py
+# Make to ignore this file when syncing with git
 # Add your credentials here before running the project
 
+# Confluence credentials to allow Nur to read from confluence and add pages to confluence
 confluence_credentials = {'base_url': '',
                           'username': '',
                           'api_token': ''
                           }
-
+# OpenAI API key
+# to be used so the solution can create embeds and use the assistant to get responses
 oai_api_key = ""
 
-# Slack tokens Toptal
+
+# Slack tokens
+# To be used to listen to messages form the channels the bot is invited to and to post answers
 slack_app_level_token = ""
 slack_bot_user_oauth_token = ""
 slack_user_oauth_token = ""
