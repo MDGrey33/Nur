51def708c20b2f9226c13da6fe8fe03ca67a46f5
Roland Abou Younes <roland@abouyounes.com>
2024-03-12 20:52:55 +0200

    Updated the configuration file. Removed the queues that are no longer used.

--- configuration.py
+++ configuration.py
@@ -33,14 +33,6 @@ sql_file_path = database_path + "/confluence_pages_sql.db"
 persist_page_processing_queue_path = os.path.join(project_path, "content", "transactional", "confluence_page_processing_queue")
 # queue for creating page vectors and storing them in chroma db
 persist_page_vector_queue_path = os.path.join(project_path, "content", "transactional", "confluence_page_vector_queue")
-# queue for slack messages
-persist_message_queue_path = os.path.join(project_path, "content", "transactional", "slack_message_queue")
-# queue for slack questions
-persist_question_queue_path = os.path.join(project_path, "content", "transactional", "slack_question_queue")
-# queue for slack reactions
-persist_feedback_queue_path = os.path.join(project_path, "content", "transactional", "slack_feedback_queue")
-# queue for qna documents
-persist_qna_document_queue_path = os.path.join(project_path, "content", "transactional", "qna_document_queue")
 
 
 
@@ -68,11 +60,10 @@ embedding_model_id = embedding_model_id_latest_large
 # page retrieval for answering questions
 # document count is recommended from 3 to 15 where 3 is minimum cost and 15 is maximum comprehensive answer
 document_count = 2
-# interaction retrieval for identifying knowledge gaps
-# interaction_retrieval_count is recommended from 3 to 10 where 3 is minimum cost and 10 is maximum comprehensive list of questions
+# interaction retrieval for identifying knowledge gaps interaction_retrieval_count is recommended from 3 to 10 where
+# 3 is minimum cost and 10 is maximum comprehensive list of questions
 interaction_retrieval_count = 5
 
-# Refactor: Might want to store only string values here cause the modules are all pulling the environment variable
 # Configuration for the Nur Services API
 # get the values from the environment variables if available or use the default values
 api_host = os.environ.get("NUR_API_HOST", "localhost")
@@ -82,11 +73,10 @@ api_port = os.environ.get("NUR_API_PORT", "8000")
 pages_collection_name = "pages"
 interactions_collection_name = "interactions"
 
-# Knowledge collection slack channel ids
+# Knowledge collection Slack channel ids
 slack_channel_priv_kb = "C06EGCDNA4A"
 slack_channel_tt_ta_debug = "C06EA5WFGUF"
 slack_channel_tt_ta = "C052GJ7GLVC"
-
 channel_id = slack_channel_priv_kb
 
 
