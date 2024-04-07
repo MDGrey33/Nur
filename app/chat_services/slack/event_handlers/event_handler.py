fcc0c0d1a1c148c074994943cb4ff3c5c3ca81e7
Roland Abou Younes <roland@abouyounes.com>
2024-04-07 03:34:09 +0300

    Added an md in documentation that highlights the raw format of each type of event Added specific event handlers using a factory

--- app/chat_services/slack/event_handlers/event_handler.py
+++ app/chat_services/slack/event_handlers/event_handler.py
@@ -4,4 +4,4 @@ from abc import ABC, abstractmethod
 class SlackEventHandler(ABC):
     @abstractmethod
     def handle(self, client, req, web_client, bot_user_id):
-        print("Handling Slack events")
+        pass
--- app/chat_services/slack/slack_service.py
+++ app/chat_services/slack/slack_service.py
@@ -1,6 +1,8 @@
 from app.chat_services.chat_service_interface import ChatServiceInterface
 from .slack_client import SlackClient
-from .channel_message_handler import ChannelMessageHandler
+from app.chat_services.slack.event_handlers.event_handler_factory import (
+    SlackEventHandlerFactory,
+)
 from slack.client import get_bot_user_id
 import logging
 
@@ -9,16 +11,22 @@ class SlackService(ChatServiceInterface):
     def start_service(self):
         bot_user_id = get_bot_user_id()
         slack_client = SlackClient()
-        event_handlers = [ChannelMessageHandler()]
+        # Initialize the EventHandlerFactory
+        event_handler_factory = SlackEventHandlerFactory()
 
-        for handler in event_handlers:
-            slack_client.socket_mode_client.socket_mode_request_listeners.append(
-                lambda _, req: handler.handle(
-                    client=slack_client.socket_mode_client,
-                    req=req,
-                    web_client=slack_client.web_client,
-                    bot_user_id=bot_user_id,
-                )
+        def handle_event(_, req):
+            # Use the factory to get the appropriate handler based on the event
+            handler = event_handler_factory.get_handler(req.payload, bot_user_id)
+            handler.handle(
+                client=slack_client.socket_mode_client,
+                req=req,
+                web_client=slack_client.web_client,
+                bot_user_id=bot_user_id,
             )
+
+        slack_client.socket_mode_client.socket_mode_request_listeners.append(
+            handle_event
+        )
+
         slack_client.socket_mode_client.connect()
         logging.info("Slack Bot has started successfully.")
