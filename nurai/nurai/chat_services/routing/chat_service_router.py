38e22a0ffa85bb93065f010c94feeedbc47c3e46
Roland Abou Younes <roland@abouyounes.com>
2024-04-08 11:17:48 +0300

    added default query for the chat service to be slack

--- nurai/nurai/chat_services/routing/chat_service_router.py
+++ nurai/nurai/chat_services/routing/chat_service_router.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from nurai.chat_services.chat_service_factory import ChatServiceFactory
 
 router = APIRouter()
 
 
-@router.post("/start-chat-service/{service_name}")
-async def start_chat_service(service_name: str):
+@router.post("/start-chat-service/")
+async def start_chat_service(service_name: str = Query("slack", description="The name of the chat service to start. "
+                                                                            "Currently, only 'slack' is supported.")):
     try:
         chat_service = ChatServiceFactory.get_service(service_name)
         chat_service.start_service()
