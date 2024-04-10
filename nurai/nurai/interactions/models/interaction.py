16c0c2a6df8c51b72113e11f48da2d71ec3bce49
Roland Abou Younes <roland@abouyounes.com>
2024-04-10 16:45:32 +0300

    Updated interactions model

--- nurai/nurai/interactions/models/interaction.py
+++ nurai/nurai/interactions/models/interaction.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
+# /nurai/interactions/models/interaction.py
+from sqlalchemy import Column, Integer, String, DateTime, Text, func
 from nurai.database.database import Base
 from nurai.database.mixins.crud_mixin import CRUDMixin
 
@@ -6,11 +7,16 @@ from nurai.database.mixins.crud_mixin import CRUDMixin
 class Interaction(Base, CRUDMixin):
     __tablename__ = "interactions"
     id = Column(Integer, primary_key=True)
-    ts = Column(String, index=True)
-    thread_ts = Column(String, index=True, nullable=True)
-    channel = Column(String)
-    user = Column(String)
-    reaction = Column(String)
+    thread_ts = Column(String, index=True)
+    question_text = Column(Text)
+    assistant_thread_id = Column(String, index=True)
+    answer_text = Column(Text)
+    channel_id = Column(String)
+    slack_user_id = Column(String)
+    question_timestamp = Column(DateTime, default=func.now())
+    answer_timestamp = Column(DateTime)
+    comments = Column(Text)
 
     def get_filter_attributes(self):
-        return ["ts", "channel", "user", "reaction"]
+        return ["question_text", "thread_id", "assistant_thread_id", "answer_text", "channel_id", "slack_user_id",
+                "question_timestamp", "answer_timestamp", "comments"]
