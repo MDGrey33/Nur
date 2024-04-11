8343fdcf69dc911a713812a64a24abbe49cf4cba
Roland Abou Younes <roland@abouyounes.com>
2024-04-11 14:51:14 +0300

    Added fetch slack thread router

--- nurai/nurai/chat_services/routing/chat_service_router.py
+++ nurai/nurai/chat_services/routing/chat_service_router.py
@@ -1,6 +1,8 @@
 # /nurai/chat_services/routing/chat_service_router.py
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException, Query, Depends
 from nurai.chat_services.chat_service_factory import ChatServiceFactory
+from nurai.chat_services.slack.slack_client import SlackClient
+
 
 router = APIRouter()
 
@@ -14,3 +16,23 @@ async def start_chat_service(service_name: str = Query("slack", description="The
         return {"message": f"{service_name} service started successfully."}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.get("/fetch-slack-thread/")
+async def fetch_slack_thread(channel: str = Query(..., description="The ID of the Slack channel."),
+                             ts: str = Query(..., description="The thread timestamp."),
+                             slack_client: SlackClient = Depends(SlackClient)):
+    """
+    Fetches a thread of messages from a specified Slack channel based on the thread timestamp and formats it for interaction input.
+    """
+    try:
+        messages = slack_client.fetch_thread_messages(channel=channel, thread_ts=ts)
+        if messages is None:
+            raise HTTPException(status_code=404, detail="Messages could not be fetched.")
+        # Transform messages to interaction input format
+        interaction_input = slack_client.transform_messages_to_interaction_input(messages, channel)
+        if interaction_input is None:
+            raise HTTPException(status_code=404, detail="Failed to transform messages into interaction input.")
+        return interaction_input
+    except Exception as e:  # Consider catching more specific exceptions
+        raise HTTPException(status_code=500, detail=str(e))
--- nurai/nurai/main.py
+++ nurai/nurai/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
-
 # Knowledge source
 from nurai.knowledge_sources.routing import knowledge_sources_router
 
@@ -13,11 +12,13 @@ from nurai.assistants.routing import assistants_router
 # Slack Bot
 from nurai.chat_services.routing import chat_service_router
 
+# Interactions
+from nurai.interactions.routing.interaction_service_router import router as interactions_router
 
 app = FastAPI()
-
 
 app.include_router(knowledge_sources_router.router)
 app.include_router(events.router)
 app.include_router(assistants_router.router)
 app.include_router(chat_service_router.router)
+app.include_router(interactions_router)
