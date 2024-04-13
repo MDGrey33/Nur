06ee1d74210427ac03f325fec06005e6ac78c2db
Roland Abou Younes <roland@abouyounes.com>
2024-04-13 11:44:53 +0300

    added .lower to aervice name input

--- nurai/nurai/chat_services/chat_service_factory.py
+++ nurai/nurai/chat_services/chat_service_factory.py
@@ -1,14 +1,12 @@
-# /app/factories/chat_service_factory.py
-
+# /nurai/factories/chat_service_factory.py
 from nurai.chat_services.slack.slack_service import SlackService
 from nurai.chat_services.chat_service_interface import ChatServiceInterface
 
 
 class ChatServiceFactory:
-
     @staticmethod
     def get_service(service_name: str) -> ChatServiceInterface:
-        if service_name == "slack":
+        if service_name.lower() == "slack":
             return SlackService()
         else:
             raise ValueError(f"Service {service_name} is not supported.")
